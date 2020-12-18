503e81d418f847f163be341ad37e07786eeedb40
JJeferson <48735241+JJeferson@users.noreply.github.com>
2020-12-18 15:56:41 -0300

    fazendo testes com query
    testes com query

--- src/main/java/com/projeto2/projeto2/repository/vendasRepository.java
+++ src/main/java/com/projeto2/projeto2/repository/vendasRepository.java
@@ -1,6 +1,12 @@
 package com.projeto2.projeto2.repository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.projeto2.projeto2.models.modelo_Vendas;
 
@@ -8,5 +14,7 @@ public interface vendasRepository extends JpaRepository<modelo_Vendas,Long> {
 
 	
 	modelo_Vendas findById(long id);
+	
+	modelo_Vendas findByDescricao(String Descricao);
 	
 }
--- src/main/java/com/projeto2/projeto2/resources/vendasResource.java
+++ src/main/java/com/projeto2/projeto2/resources/vendasResource.java
@@ -1,18 +1,25 @@
 package com.projeto2.projeto2.resources;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto2.projeto2.models.modelo_Vendas;
@@ -20,6 +27,7 @@ import com.projeto2.projeto2.repository.vendasRepository;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
 
 @RestController
 @RequestMapping(value="/sistema")
@@ -64,6 +72,21 @@ public class vendasResource {
 		 }
 		
 		 */
+		
+	}
+	
+	
+	
+	//@GetMapping("/relvendasdesc")
+	@PutMapping("/vendas")
+	@ApiOperation(value="Retorna vendas baseado na Descrição")
+	public ResponseEntity<modelo_Vendas> listaVendaDesc(@RequestParam(value="Descricao", defaultValue = "") String Descricao){
+	
+	
+	modelo_Vendas Vendas = vendasrepository.findByDescricao(Descricao);	
+		
+	return ResponseEntity.ok(Vendas);
+		 
 		
 	}
 }
